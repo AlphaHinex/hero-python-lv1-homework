1a55339ee3650e0e978ec7f17e5cc044b7f444c3
Alpha Hinex <AlphaHinex@gmail.com>
2021-11-02 02:36:23 +0800

    Add 02

---
+++ 02/homework/01.py
@@ -0,0 +1,5 @@
+#conding:utf-8
+#声明变量name，存储自己的名字的笔画数量，并在控制台输出
+
+name=7+8+14
+print(name)
---
+++ 02/practice/01.py
@@ -0,0 +1,7 @@
+#conding:utf-8
+#声明变量grade，存储自己的年级，并在控制台输出
+grade = '初中一年级'
+#声明变量number，存储自己的学号，并在控制台输出
+number = 10
+print(grade)
+print(number)
