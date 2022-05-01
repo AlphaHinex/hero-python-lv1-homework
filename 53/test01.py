bcf869c46d3b08381b8c3ee4e6af9c4ef7b69000
Alpha Hinex <AlphaHinex@gmail.com>
2022-05-01 19:03:29 +0800

    Before homework

---
+++ 53/test01.py
@@ -0,0 +1,4 @@
+# coding:utf-8
+import  time
+t = time.time()
+print(t)
---
+++ 53/test02.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+lastTime = 30
+currentTine = 60
+interval = 30
+if currentTine - lastTime >= interval:
+    print('👀十分钟📺')
+else:
+    print('继续学习')
