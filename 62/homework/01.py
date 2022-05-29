c383cc8dfd1d4e6b89f4eef80b569739a3105a1b
Alpha Hinex <AlphaHinex@gmail.com>
2022-05-29 19:57:59 +0800

    Homework

--- 62/homework/01.py
+++ 62/homework/01.py
@@ -1,4 +1,28 @@
-#coding:utf-8
-              
-        
-        
+# coding:utf-8
+
+
+class Person(object):
+    def __init__(self, type):
+        self.type = type
+
+    def id(self):
+        print('我是好人')
+
+
+class Police(Person):
+    def __init__(self, type):
+        Person.__init__(self, type)
+
+
+class Killer(Person):
+    def __init__(self, type):
+        Person.__init__(self, type)
+
+    def id(self):
+        print('我是坏人')
+
+
+police = Police('警察')
+killer = Killer('杀手')
+police.id()
+killer.id()
