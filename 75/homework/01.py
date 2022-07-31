f9f54d830cd7d3b726d2025c3392a42587637d4d
Alpha Hinex <AlphaHinex@gmail.com>
2022-07-31 13:27:44 +0800

    Before replace

---
+++ 75/homework/01.py
@@ -0,0 +1,3 @@
+#coding:utf-8
+#已知字典dict1 = {'class1':'python', 'class2':'english','calss3':'maths'}
+#获取class1的值并在控制台中输出
---
+++ 75/practise/02.py
@@ -0,0 +1,3 @@
+#coding:utf-8
+#创建字典dict = {'Name': 'Runoob', 'Age': 7, 'Class': 'First'}
+#通过key获取dict中所有value值 ,将结果在控制台显示
