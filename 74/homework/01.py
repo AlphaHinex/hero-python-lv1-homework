5001bb0d22f52fe53300df2668c6ea113d0ad740
Alpha Hinex <AlphaHinex@gmail.com>
2022-08-14 22:21:13 +0800

    Add xmind

--- 74/homework/01.py
+++ 74/homework/01.py
@@ -5,3 +5,13 @@ import easygui
 # 访问car对象的属性，并显示在信息提示框上
 
 
+class Car():
+    def __init__(self, type, color, number):
+        self.type = type
+        self.color = color
+        self.number = number
+
+
+car = Car('丰田', '白色', '辽A81810')
+
+easygui.msgbox(car.type + car.color + car.number)
